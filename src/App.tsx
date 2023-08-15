b3a3f30942acbe9b840bf571b12ba6ce909f83a4
Sye-5pace <owusilasyp00@gmail.com>
2023-08-15 16:22:19 +0100

    Project setup created

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import './Index.css'
+
+const App = () => {
+
+
+  return (
+    <div className='w-full min-h-screen p-0 m-0 border-box bg-[#010044]'>
+      <h1 className='text-2xl text-[#905c02] font-antonio'>Planets fact site: font-antonio</h1>
+      <h1 className='text-2xl text-[#905c02] font-spartan'>Planets fact site: Font-spartan</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'alabaster':'#FAFAFA',
+      'woodsmoke':'#111517',
+      'silver': '#c1c1c1',
+      'gray': '#848484',
+      'ebonyclaylight':'#202C36',
+      'ebonyclaydark':'#2B3844',
+      'white': '#fff'
+    },
+    screens: {
+      'mobile': {'min':'200px','max':'427px'},
+      'tablet': {'min':'427px','max':'782px'},
+      'laptop': {'min':'782.1px'}
+    },
+    extend: {
+      fontFamily:{
+        antonio: ['Antonio', 'sans-serif'],
+        spartan: ['League Spartan', 'sans-serif']
+      }
+    },
+    boxShadow: {
+      'custom': '16px 6px 16px rgba(30, 44, 52, 1)',
+    },
+  },
+  plugins: [
+    plugin(function({addVariant}){
+      addVariant("children","&>*")
+    })
+  ],
+}
